c4435bb555b7b487badacf6159991dbce871cfba
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-25 23:18:53 +0200

    move OurCrew into its own compo and add description text

---
+++ app/about_us/OurCrew.js
@@ -0,0 +1,13 @@
+const OurCrew = () => {
+  return (
+    <>
+      <h1>Who we are</h1>
+      <p>
+        Our crew is the heart and soul of Galactica. We are a diverse team of
+        seasoned space explorers, engineers, and visionaries who are united by a
+        common goal: to make space travel accessible and exciting for all.
+      </p>
+    </>
+  );
+};
+export default OurCrew;
