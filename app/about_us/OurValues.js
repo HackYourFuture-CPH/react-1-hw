da447d3757f849b3e31946a91aced16e8bd73bfe
FurkannOzbek <furkannozbek@yandex.com>
2024-08-26 19:04:36 +0200

    About us section redesigned

--- app/about_us/OurValues.js
+++ app/about_us/OurValues.js
@@ -1,5 +1,44 @@
 const OurValues = () => {
-  return <p> ADD OUR VALUES HERE </p>;
+  return (
+    <div className="value-containers">
+      <div className="value-container">
+        <img className="value-icons" src="/icons/exploration.png"></img>
+        <h2 className="value-title">Exploration</h2>
+        <p className="value-text">
+          We are driven by a deep-seated desire to explore the unknown. We believe that the pursuit
+          of discovery is at the heart of human nature, and we are committed to pushing the
+          boundaries of what is possible.
+        </p>
+      </div>
+      <div className="value-container">
+        <img className="value-icons" src="/icons/innovation.png"></img>
+        <h2 className="value-title">Innovation</h2>
+        <p className="value-text">
+          At Galactica, we prioritize cutting-edge technology and innovation. We are constantly
+          evolving our spacecraft, safety protocols, and services to ensure that our travelers
+          experience the most advanced and secure space journeys available.
+        </p>
+      </div>
+      <div className="value-container">
+        <img className="value-icons" src="/icons/sustainability.png"></img>
+        <h2 className="value-title">Sustainability</h2>
+        <p className="value-text">
+          We are committed to making space exploration sustainable for future generations. Our space
+          missions are designed to minimize environmental impact, both on Earth and in space, and to
+          foster a spirit of responsibility towards our universe.
+        </p>
+      </div>
+      <div className="value-container">
+        <img className="value-icons" src="/icons/community.png"></img>
+        <h2 className="value-title">Community</h2>
+        <p className="value-text">
+          We believe in the power of collective exploration. Our journeys are not just about
+          reaching new destinations; they are about building a community of space enthusiasts who
+          share a passion for the stars.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default OurValues;
