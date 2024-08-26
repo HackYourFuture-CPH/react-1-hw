4608a1568d55a4bdd8c932080111b236f9798675
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 13:46:47 +0200

    remove unnecessary items

--- app/about_us/OurPartners.jsx
+++ app/about_us/OurPartners.jsx
@@ -4,13 +4,12 @@ const partners = [
   { partner: "CBC", logo: "/business_partners/CBC_Logo_White.png" },
   { partner: "Microsoft", logo: "/business_partners/Microsoft-Logo-white.png" },
   { partner: "nyu", logo: "/business_partners/nyu-logo.png" },
-  { partner: "Queens", logo: "/business_partners/QueensLogo_white.ong" },
+  { partner: "Queens", logo: "/business_partners/QueensLogo_white.png" },
   { partner: "samsung", logo: "/business_partners/samsung-logo.png" },
   { partner: "sodexo", logo: "/business_partners/sodexo-logo.png" },
 ];
 
 const OurPartners = () => {
-  // Some inspiration ideas found in /data/inspiration_about_us
   return (
     <>
       <p>
@@ -18,9 +17,9 @@ const OurPartners = () => {
         technology industries to make every journey extraordinary.
       </p>
       <ul>
-        {partners.map((image,index) => (
+        {partners.map((image, index) => (
           <li key={index}>
-            <img src={image.logo} alt={image.partner} />;
+            <img src={image.logo} alt={image.partner} />
           </li>
         ))}
       </ul>
