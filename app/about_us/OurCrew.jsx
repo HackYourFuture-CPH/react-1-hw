9ba9ed0e529f45be965527bea8ede10a1121f837
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 12:33:31 +0200

    separate title and name

--- app/about_us/OurCrew.jsx
+++ app/about_us/OurCrew.jsx
@@ -2,31 +2,36 @@ import styles from "./OurCrew.module.css";
 
 const crew = [
   {
-    titleAndName: "Captain Sarah Vega",
+    name: "Sarah Vega",
+    title: "Captain",
     duties:
       "A former NASA astronaut with over 15 years of experience, Captain Vega leads our missions with unparalleled expertise and a passion for space exploration.",
     image: "/crew/image-anousheh-ansari.png",
   },
   {
-    titleAndName: "Dr. Leo Redding",
+    name: "Leo Redding",
+    title: "Dr.",
     duties:
       "Our chief astrophysicist, Dr. Redding, is a renowned scientist who has contributed to major space discoveries. He ensures that every journey is as educational as it is exhilarating.",
     image: "/crew/image-douglas-hurley.png",
   },
   {
-    titleAndName: "Chief Engineer Hana Lee",
+    name: "Hana Lee",
+    title: "Chief Engineer",
     duties:
       "With her extensive background in aerospace engineering, Hana Lee is responsible for the state-of-the-art technology that powers our spacecraft. Her innovation ensures that our travelers are always in safe hands.",
     image: "/crew/image-mark-shuttleworth.png",
   },
   {
-    titleAndName: "Mission Specialist Alex Santos",
+    name: "Alex Santos",
+    title: "Mission Specialist",
     duties:
       "As a mission specialist, Alex’s job is to ensure that every aspect of the journey runs smoothly. With a background in both science and adventure tourism, Alex is the perfect guide for our space travelers.",
     image: "/crew/image-victor-glover.png",
   },
   {
-    titleAndName: "Crew Member Maya Patel",
+    name: "Maya Patel",
+    title: "Crew Member",
     duties:
       "Maya brings a unique blend of technical skills and customer service experience to the team. She’s always ready to assist with any needs and to make sure every traveler has an unforgettable experience.",
     image: "/crew/image-victor-glover.png",
@@ -39,8 +44,9 @@ const OurCrew = () => {
       <ul className={styles.ulContainer}>
         {crew.map((member) => (
           <li className={styles.listItem}>
-            <div>{member.titleAndName}</div>
-            <div >{member.duties}</div>
+            <div className={styles.name}>{member.name}</div>
+            <div className={styles.title}>{member.title}</div>
+            <div className={styles.duties}>{member.duties}</div>
             <img
               src={member.image}
               alt={member.titleAndName}
