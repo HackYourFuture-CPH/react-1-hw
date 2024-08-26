bdbdbdaed976712aa84be184ac7ac0c9c5bff16f
FurkannOzbek <furkannozbek@yandex.com>
2024-08-26 21:30:34 +0200

    small fix

--- app/about_us/OurMission.js
+++ app/about_us/OurMission.js
@@ -3,7 +3,6 @@ import styles from "./OurMission.module.css";
 const OurMission = () => {
   return (
     <p className={styles["mission-p"]}>
-      {" "}
       <br></br> At Galactica, our mission is to unlock the wonders of the universe for everyone. We
       believe that space is the final frontier and that the opportunity to explore it should be
       within everyone’s reach. Our journeys are designed to inspire, educate, and provide a
