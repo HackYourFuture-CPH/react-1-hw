ad50fdf44dbd3e2b376ac0cf87bde111c5debe53
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 13:42:42 +0200

    convert partner PNGs into an array; map over the PNGs in OurPartners compo

--- app/about_us/OurPartners.jsx
+++ app/about_us/OurPartners.jsx
@@ -1,14 +1,30 @@
+const partners = [
+  { partner: "alphabet", logo: "/business_partners/alphabet-logo.png" },
+  { partner: "amazon", logo: "/business_partners/amazon_logo.png" },
+  { partner: "CBC", logo: "/business_partners/CBC_Logo_White.png" },
+  { partner: "Microsoft", logo: "/business_partners/Microsoft-Logo-white.png" },
+  { partner: "nyu", logo: "/business_partners/nyu-logo.png" },
+  { partner: "Queens", logo: "/business_partners/QueensLogo_white.ong" },
+  { partner: "samsung", logo: "/business_partners/samsung-logo.png" },
+  { partner: "sodexo", logo: "/business_partners/sodexo-logo.png" },
+];
+
 const OurPartners = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Crew section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Use the pictures from /public/crew
   // Some inspiration ideas found in /data/inspiration_about_us
   return (
-    <p>
-      We collaborate with some of the most respected names in the space and
-      technology industries to make every journey extraordinary.
-    </p>
+    <>
+      <p>
+        We collaborate with some of the most respected names in the space and
+        technology industries to make every journey extraordinary.
+      </p>
+      <ul>
+        {partners.map((image) => (
+          <li>
+            <img src={image.logo} alt={image.partner} />;
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
