34ec7b4caa325a1ea14c86e16927b9b112935e79
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-03 18:45:06 +0200

    style the SocialMediaItem list

--- components/ui/SocialMediaItem.jsx
+++ components/ui/SocialMediaItem.jsx
@@ -1,9 +1,10 @@
+import styles from "./Footer.module.css";
 function SocialMediaItem({ url, title, icon }) {
   return (
-    <li>
+    <li className={styles.socialListItem}>
       <a href={url}>
-        <img src={icon} alt={`${title} icon`} className="" />
-        {title}
+        <img src={icon} alt={`${title} icon`} className={styles.socialIcon} />
+        <span>{title}</span>
       </a>
     </li>
   );
