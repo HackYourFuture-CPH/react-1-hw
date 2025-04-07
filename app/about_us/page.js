39742da397a8dc7b2dfe75aa209698a0640d56df
Stefany <Stefanyromerov88@gmail.com>
2025-04-07 08:48:49 +0200

    update

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -7,6 +7,7 @@ import styles from './page.module.css';
 
 const OurValues = () => {
   // TASK - React 1 week 1
+
   // Create the "Our Values" section
   // Use the descriptions provided in /app/about_us/README.md
   // Some inspiration ideas found in /data/inspiration_about_us
