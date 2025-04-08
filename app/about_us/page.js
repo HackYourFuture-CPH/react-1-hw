1e4051eb8e06c58328bf594aa044ad188c8a6703
Stefany <Stefanyromerov88@gmail.com>
2025-04-08 09:26:35 +0200

    ip

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,9 +1,8 @@
 import styles from './page.module.css';
+import OurCrew from './OurCrew';
+import OurPartners from './OurPartners';
+import OurValues from './OurValues';
 
-// TASK - React 1 week 1
-// After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
-// OurValues.js, OurCrew.js, OurPartners.js should live in this folder
-// import and use the components from the newly created files
 
 const OurValues = () => {
   // TASK - React 1 week 1
